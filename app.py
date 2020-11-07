dab770185e5578ca5e8c73c85d3d733c790b3c73
love-adelar <love.adelar@gmail.com>
2020-11-07 14:41:06 +0900

    app.py: 수정 API 구현
    LIST API에서 _id 도 함께 반환함

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask, render_template, jsonify, request
 from pymongo import MongoClient
+from bson import ObjectId
+from json import JSONEncoder
+
+class CustomJSONEncoder(JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return JSONEncoder.default(self, o)
 
 app = Flask(__name__)
+app.json_encoder = CustomJSONEncoder
+
 db = MongoClient('localhost', 27017).dbJungleTest
 
 @app.route('/')
@@ -10,7 +20,7 @@ def index():
 
 @app.route('/memo', methods=['GET'])
 def get_articles():
-    result = list(db.articles.find({}, {'_id':0}))
+    result = list(db.articles.find())
     return jsonify({'result': 'success', 'articles': result})
 
 @app.route('/memo', methods=['POST'])
@@ -19,11 +29,31 @@ def post_articles():
     if not (article['title'] and article['content']):
         return jsonify({'result': 'failed'}), 400
 
+    # TODO: 에러 핸들링
     db.articles.insert_one({
         'title': article['title'],
         'content': article['content']
     })
     return jsonify({'result': 'success', 'msg':'글이 작성되었습니다.'})
+
+@app.route('/memo/<id>', methods=['PUT'])
+def update_articles(id):
+    article = request.get_json()
+    if not (article['title'] and article['content']):
+        return jsonify({'result': 'failed'}), 400
+
+    result = db.articles.update_one({'_id': ObjectId(id)},
+        {
+            '$set': {
+                'title': article['title'],
+                'content': article['content']
+            }
+        }
+    )
+    if result.modified_count == 0:
+        return jsonify({'result': 'failed'}), 400
+
+    return jsonify({'result': 'success', 'msg':'글이 수정되었습니다.'})
 
 if __name__ == "__main__":
     app.run('0.0.0.0', port=5000, debug=True)
